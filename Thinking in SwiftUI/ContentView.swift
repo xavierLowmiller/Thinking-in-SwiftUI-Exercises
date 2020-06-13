a20ef5cb490d8fdca842ba35671fd7afea340f83
Xavier Lowmiller <xaver.lohmueller@me.com>
2020-06-13 12:17:29 +0200

    Initial Commit

---
+++ Thinking in SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Thinking in SwiftUI
+//
+//  Created by Xaver Lohmüller on 13.06.20.
+//  Copyright © 2020 Xaver Lohmüller. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
