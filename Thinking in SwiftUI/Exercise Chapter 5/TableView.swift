ff48b61d6e776004ccec82ecd4e56f7f0b9da349
Xavier Lowmiller <xaver.lohmueller@me.com>
2020-06-26 11:23:15 +0200

    Chapter 5 Exercises: Animated transition when selecting

--- Thinking in SwiftUI/Exercise Chapter 5/TableView.swift
+++ Thinking in SwiftUI/Exercise Chapter 5/TableView.swift
@@ -38,38 +38,60 @@ struct CollectMaxHeight: ViewModifier {
 	}
 }
 
+private struct Rect {
+	let origin: (x: CGFloat, y: CGFloat)
+	let size: (width: CGFloat?, height: CGFloat?)
+	static let empty = Rect(origin: (0, 0), size: (nil, nil))
+}
+
 struct Table<Content: View>: View {
 	let cells: [[Content]]
+	let xSpacing: CGFloat = 8
+	let ySpacing: CGFloat = 0
 	@State private var widths: [Int: CGFloat] = [:]
 	@State private var heights: [Int: CGFloat] = [:]
-	@State private var selected: (row: Int, column: Int)?
+	@State private var selected: (row: Int, column: Int)? // = (2, 1)
 
-	func isSelected(row: Int, column: Int) -> Bool {
-		if let selected = selected {
-			return row == selected.row && column == selected.column
-		} else {
-			return false
-		}
+	private var frameForSelectedCell: Rect {
+		guard let selected = selected else { return .empty }
+
+		return Rect(
+			origin: (
+				(0..<selected.column).map { (widths[$0] ?? 0) + xSpacing }.reduce(0, +),
+				(0..<selected.row).map { (heights[$0] ?? 0) + ySpacing }.reduce(0, +)
+			), size: (
+				widths[selected.column],
+				heights[selected.row]
+			)
+		)
 	}
 
 	var body: some View {
-		VStack {
+		VStack(spacing: ySpacing) {
 			ForEach(cells.indices) { row in
-				HStack {
+				HStack(spacing: xSpacing) {
 					ForEach(cells[row].indices) { column in
 						cells[row][column]
 							.padding(2)
 							.modifier(CollectMaxWidth(index: column))
 							.modifier(CollectMaxHeight(index: row))
 							.frame(minWidth: widths[column], minHeight: heights[row], alignment: .leading)
-							.border(isSelected(row: row, column: column) ? Color.blue : Color.clear, width: 2)
 							.onTapGesture { withAnimation { selected = (row, column) } }
 					}
-				}.background(row.isMultiple(of: 2) ? Color.white : Color(.systemGroupedBackground))
+				}.background(row.isMultiple(of: 2) ? .white : Color(.systemGroupedBackground))
 			}
 		}
 		.onPreferenceChange(WidthPreference.self) { widths = $0 }
 		.onPreferenceChange(HeightPreference.self) { heights = $0 }
+		.overlay(ZStack(alignment: .topLeading) {
+			Color.clear
+			Color.clear
+				.border(selected == nil ? Color.clear : .blue, width: 2)
+				.offset(x: frameForSelectedCell.origin.x,
+								y: frameForSelectedCell.origin.y)
+				.frame(width: frameForSelectedCell.size.width,
+							 height: frameForSelectedCell.size.height)
+		})
 	}
 }
 
